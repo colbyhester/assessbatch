6efd4d00a69f1e52fe20c84b6f76e69049cb8648
lutimothy13 <lutimothy13@gmail.com>
2018-11-06 20:32:51 -0500

    Merged the streams

---
+++ ColumnBtnController.js
@@ -0,0 +1,22 @@
+({
+    closeModal:function(component,event,helper){    
+        var cmpTarget = component.find('Modalbox');
+        var cmpBack = component.find('Modalbackdrop');
+        $A.util.removeClass(cmpBack,'slds-backdrop--open');
+        $A.util.removeClass(cmpTarget, 'slds-fade-in-open'); 
+    },
+    openmodal:function(component,event,helper) {
+        var cmpTarget = component.find('Modalbox');
+        var cmpBack = component.find('Modalbackdrop');
+        $A.util.addClass(cmpTarget, 'slds-fade-in-open');
+        $A.util.addClass(cmpBack, 'slds-backdrop--open'); 
+    },
+    
+ doInit : function(cmp) {
+     
+    var table = document.getElementById("Colbysucks");
+    var cell = table.insertCell(1);
+    cell.innerHTML = "<b>Apex Test</b>";
+    
+}
+ })
