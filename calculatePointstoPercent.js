a24a72c964f7e2a3a7dfb2b3f3c62d25cdddf2f7
Dinahlgo <dl03047@georgiasouthern.edu>
2018-11-06 22:53:16 -0500

    hoh my good texas gravy

--- calculatePointstoPercent.js
+++ calculatePointstoPercent.js
@@ -1,7 +1,7 @@
 /*points to percent*/
 ({
 	pointsToPercent : function(Assessment, Index) {
-   	/*numerator */
+   	/*grab the number of points from index*/
         var TrainerPoints= Assessment[Index].Max_Points__c;
         /*total points*/
         var Pointssum=0;
@@ -11,5 +11,19 @@
   		return (TrainerPoints/Pointssum)*100;
   }
         
-    }
+    }//end of points to percent*/
+    sectionAverage : function(Score__c){
+        /*iterate through the grades*/
+        ScoresSum=0;
+        for(i=0; i<Score__c.length; i++){
+       /*just an array of integers; sums it up*/
+            ScoresSum += Score__c[i];
+    }/*we want to return the average*/
+    return(GradesSum/Score__c.length);
+    }//end of sectionAverage
+
+    weeklyBatchAverage: function(Score__c){
+
+      
+    }//end of weeklyAverage
 })
